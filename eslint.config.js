495cee823a04efa980b2442b72fa93c497a94981
test23780460 <133947882+test23780460@users.noreply.github.com>
2026-06-14 10:36:31 -0400

    Add ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { project: "./tsconfig.json" },
+      globals: { window: "readonly", document: "readonly", localStorage: "readonly", fetch: "readonly", console: "readonly", process: "readonly", location: "readonly", React: "readonly" }
+    },
+    plugins: { "@typescript-eslint": tseslint, "react-hooks": reactHooks },
+    rules: { ...tseslint.configs.recommended.rules, ...reactHooks.configs.recommended.rules, "@typescript-eslint/no-explicit-any": "off" }
+  }
+];
